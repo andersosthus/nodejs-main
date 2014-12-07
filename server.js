05618503914c790f65a4f4bd9527236a145e94c5
Anders Østhus <anders.osthus@outlook.com>
2014-12-07 19:06:26 +0100

    Added basic server

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var startTime = new Date();
+
+app.get('/', function(req, res) {
+  var elapsedTime = new Date().getTime() - startTime.getTime();
+  console.log('request recieved, sending ' + elapsedTime.toString());
+  res.end('This server has been running for ' + elapsedTime + 'ms.\n');
+});
+
+var port = process.env.POST || 5000;
+server = app.listen(port);
+
+console.log('Server listening on port %d in %s mode', server.address().port, app.settings.env);
